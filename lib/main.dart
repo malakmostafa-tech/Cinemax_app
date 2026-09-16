66d45138a5454aa53806a7c55f04bd537aa674c5
malokaa234. <malk69391@gmail.com>
2026-09-16 07:45:57 +0300

    main modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:cinemax_app/core/app_theme.dart';
-import 'package:cinemax_app/features/movie/presentation/cubit/movie_cubit.dart';
-import 'package:cinemax_app/features/movie/presentation/pages/main_wrapper_screen.dart';
+import 'package:cinemax_app/features/onboarding/presentation/pages/onboarding_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: 'api.env');
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-    ),
-  );
-
-  runApp(const CinemaxApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class CinemaxApp extends StatelessWidget {
-  final MovieCubit? cubit;
-
-  const CinemaxApp({super.key, this.cubit});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => cubit ?? MovieCubit(),
-      child: MaterialApp(
-        title: 'Cinemax',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.dark,
-        home: const MainWrapperScreen(),
-      ),
+    return MaterialApp(
+      title: 'CINEMAX',
+      debugShowCheckedModeBanner: false,
+      home: const OnboardingScreen(),
     );
   }
 }
